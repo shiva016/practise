c45a8ede7033bb13244cb2b946429c1a47083ee6
Shiva Lellapati <shivalellapati@Shivas-MacBook-Air.local>
2020-08-08 13:37:32 -0400

    add odd number output

--- even.py
+++ even.py
@@ -1,5 +1,15 @@
 #lets fing an even number
+evennums = []
+oddnums = []
 for i in range(100):
     if i%2 == 0:
-        print(f'{i} is the even number')
+        evennums.append(i)
+    else:
+        oddnums.append(i)
+        print("===========================================================")
+        print(evennums)
+        print(65 * "==")
+        print(oddnums)
+
+
        
